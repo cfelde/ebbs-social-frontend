ad47de958f3e60bbafe0867f3e192981d3acf3d6
Christian Felde <cfelde@cfelde.com>
2020-01-02 10:42:11 +0100

    Fix project.clj license reference

--- project.clj
+++ project.clj
@@ -1,8 +1,8 @@
 (defproject ebbs-social-frontend "0.1.0-SNAPSHOT"
   :description "Web frontend for EBBS social"
   :url "https://ebbs.social"
-  :license {:name "Eclipse Public License"
-            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :license {:name "Apache License Version 2.0"
+            :url "http://www.apache.org/licenses/"}
 
   :min-lein-version "2.9.1"
 
